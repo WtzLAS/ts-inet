6c17fe0581f9973948dbe3cc918e9310ee773347
Wtz_LASR <wtz_lasr@outlook.com>
2022-09-11 00:21:34 +0800

    init

---
+++ src/core.ts
@@ -0,0 +1,75 @@
+export type Agent = Name | Normal;
+type Name = { port: Agent | null };
+export type Normal = { id: number, ports: Agent[] };
+
+export function new_name(): Agent {
+    return { port: null };
+}
+
+export function new_agent(id: number, ports: Agent[]): Agent {
+    return { id, ports };
+}
+
+export type RuleFn = (machine: Machine, lhs: Normal, rhs: Normal) => void;
+
+export class Machine {
+    eqs: [Agent, Agent][];
+    rules: Map<string, RuleFn>;
+
+    public constructor() {
+        this.eqs = [];
+        this.rules = new Map();
+    }
+
+    public add_eq(lhs: Agent, rhs: Agent) {
+        this.eqs.push([lhs, rhs]);
+    }
+
+    public add_rule(id_lhs: number, id_rhs: number, rule: RuleFn) {
+        if (this.rules.has([id_lhs, id_rhs].toString()) || this.rules.has([id_rhs, id_lhs].toString())) {
+            throw new Error("duplicate rule");
+        }
+        this.rules.set([id_lhs, id_rhs].toString(), rule);
+        this.rules.set([id_rhs, id_lhs].toString(), function (m, l, r) {
+            rule(m, r, l)
+        });
+    }
+
+    public run(): [number, number] {
+        let op_interaction = 0;
+        let op_name = 0;
+        while (true) {
+            let eq = this.eqs.pop();
+            if (eq == undefined) {
+                break;
+            }
+            let [lhs, rhs] = eq;
+            if ("port" in rhs) {
+                // rhs is Name
+                if (rhs.port == null) {
+                    rhs.port = lhs;
+                } else {
+                    this.eqs.push([lhs, rhs.port]);
+                }
+                op_name++;
+            } else if ("port" in lhs) {
+                // lhs is Name
+                if (lhs.port == null) {
+                    lhs.port = rhs;
+                } else {
+                    this.eqs.push([lhs.port, rhs]);
+                }
+                op_name++;
+            } else {
+                // lhs & rhs are Normal
+                let rule = this.rules.get([lhs.id, rhs.id].toString());
+                if (rule == undefined) {
+                    throw new Error("no rule");
+                }
+                rule(this, lhs, rhs);
+                op_interaction++;
+            }
+        }
+        return [op_interaction, op_name];
+    }
+}
---
+++ src/main.ts
@@ -0,0 +1,61 @@
+import { Agent, Machine, new_agent, new_name } from "./core";
+
+const Z_ID = 0;
+const S_ID = 1;
+const ADD_ID = 2;
+
+function add_number(machine: Machine, n: number): Agent {
+    let name = new_name();
+    let last_port = name;
+    while (n > 0) {
+        let aux_port = new_name();
+        machine.add_eq(new_agent(S_ID, [last_port, aux_port]), last_port);
+        last_port = aux_port;
+        n--;
+    }
+    machine.add_eq(new_agent(Z_ID, [last_port]), last_port);
+    return name;
+}
+
+let machine = new Machine();
+
+machine.add_rule(ADD_ID, S_ID, function (m, l, r) {
+    let name = new_name();
+    m.add_eq(new_agent(ADD_ID, [r.ports[1], l.ports[1], name]), r.ports[1]);
+    m.add_eq(new_agent(S_ID, [l.ports[2], name]), l.ports[2]);
+});
+
+machine.add_rule(ADD_ID, Z_ID, function (m, l, r) {
+    m.add_eq(l.ports[1], l.ports[2]);
+});
+
+let x = add_number(machine, 10000);
+let y = add_number(machine, 10000);
+let o = new_name();
+
+machine.add_eq(new_agent(ADD_ID, [x, y, o]), x);
+
+console.log(machine.run());
+
+let tmp = 0;
+
+while (true) {
+    if ("port" in o) {
+        if (o.port == null) {
+            throw new Error("inconsistent state");
+        } else {
+            o = o.port;
+        }
+    } else {
+        if (o.id == S_ID) {
+            tmp++;
+            o = o.ports[1];
+        } else if (o.id == Z_ID) {
+            break;
+        } else {
+            throw new Error("inconsistent state");
+        }
+    }
+}
+
+console.log(tmp);
